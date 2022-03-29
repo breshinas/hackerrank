6976b86482658390cc8284b9ca6ca1ce610bf047
breshinas <9035788472@mail.ru>
2022-03-29 08:35:36 +0300

    Diagonal Difference https://www.hackerrank.com/challenges/diagonal-difference

---
+++ DiagonalDifference.cs
@@ -0,0 +1,52 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static int diagonalDifference(List<List<int>> arr)
+    {
+        var sum1 = 0;
+        var sum2 = 0;
+        var count = arr[0].Count;
+        for(var i=0;i<count;i++){
+            sum1 += arr[i][i];
+            sum2 += arr[i][count-i-1];
+        }
+        return Math.Abs(sum1-sum2);
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<List<int>> arr = new List<List<int>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+        }
+
+        int result = Result.diagonalDifference(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
